38719008a528cb29e943b1beaf23eac8fcd97c4b
JmcPortal12 <jmcstaffassociationportal@gmail.com>
2025-10-20 16:26:19 +0530

    add upload folder

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-   process.env.BASE_DATABASE,
-   process.env.BASE_USER,
-   process.env.BASE_PASSWORD, 
-   {
-  host: process.env.BASE_URL,
-  dialect: 'mysql',
-  logging: false,
-});
+  process.env.BASE_DATABASE,
+  process.env.BASE_USER,
+  process.env.BASE_PASSWORD,
+  {
+    host: process.env.BASE_URL,
+    port: process.env.BASE_PORT || 3306,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
 sequelize.authenticate()
   .then(() => console.log('✅ Connected to MySQL successfully...'))
