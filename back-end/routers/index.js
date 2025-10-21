436391683fda0fb13d324489b6dfeb5167c50288
MOHAMED FAWAZ <155258376+MohamedFawaz14@users.noreply.github.com>
2025-10-21 13:54:19 +0530

    updated contract form with brevo service

--- back-end/routers/index.js
+++ back-end/routers/index.js
@@ -45,9 +45,9 @@ router.post('/contactForm', async (req, res) => {
     const { email, name, phone, type } = formData;
     
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      host:"smtp-relay.brevo.com",
+      port:587,
+      secure:false,
       auth: {
         user: 'process.env.EMAIL_USER',
         pass: 'process.env.EMAIL_PASSWORD',
